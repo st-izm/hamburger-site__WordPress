12a629c9909d1370d0490d50d740cf29d8bb1c13
st-izm <shun00305765@gmail.com>
2021-04-27 23:42:20 +0900

    20210427_開発途上_search.php編集

--- search.php
+++ search.php
@@ -13,6 +13,37 @@
         <dd class="p-archive__top__description">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
     </dl>
 
+
+
+
+
+<?php
+// 引数の例
+$paged = get_query_var('paged')? get_query_var('paged') : 1;
+// クエリを実行する
+$the_query = new WP_Query( array(
+    'post_type' => 'post',
+    'paged' => $paged,
+    'post_status' => 'publish',
+    'posts_per_page' => 5,
+));
+?>
+<?php if ( $the_query->have_posts() ) : ?>
+
+<!-- 投稿の検索結果表示のループ処理 -->
+        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            
+            <?php get_template_part( 'search', 'archive' ); ?> <!-- get_template_part();で search-archive.php のテンプレートファイルを参照にするよう指定した。 -->
+            <?php wp_link_pages( 'before=<div class="c-pager">&after=</div>' ); ?>
+        <?php endwhile; ?>
+        <?php wp_pagenavi(); ?>
+        <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+            <a>検索結果がありません</a>  
+    <?php endif; ?>
+
+<p>以上試作コード　以下保存コード</p>
+
 <!-- 投稿の検索結果表示のループ処理 -->
     <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : the_post(); ?>
